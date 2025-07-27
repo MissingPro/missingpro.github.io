ba5467fe5b7c81700e9b98a176d99db4c1cb01fc
MissingPro <missingpromc@Outlook.com>
2025-07-27 13:32:12 -0400

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+    $file = 'emails.txt';
+    file_put_contents($file, $email . PHP_EOL, FILE_APPEND);
+    echo 'Email saved!';
+} else {
+    echo 'No email provided.';
+}
+?>
